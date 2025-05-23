8f86cacbafe5d7e8cf05cf9eadcacf0cb5736de5
Klascal <158731903+klascal@users.noreply.github.com>
2025-05-23 14:18:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ app.post("/sso", async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
