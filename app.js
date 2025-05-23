1d8fa67bef3786cc5b0189260521e868f77e7d8e
Klascal <158731903+klascal@users.noreply.github.com>
2025-05-23 15:32:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -60,3 +60,8 @@ app.post("/", async (req, res) => {
 
   return res.json({ access_token: user.access_token });
 });
+
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
